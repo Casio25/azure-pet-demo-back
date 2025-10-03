0d15ba1c722a09f7cb61a7c5b48b9ba71f9d5e9c
Casio25 <mishakolomiets355@gmail.com>
2025-10-03 16:26:09 +0300

    frontend connection in CORS added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONT_ORIGIN ?? 'http://localhost:3001'],
+    origin: [process.env.FRONT_ORIGIN ?? 'http://localhost:3001',
+      "https://jolly-dune-0d2eebb03.1.azurestaticapps.net"
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4001);
